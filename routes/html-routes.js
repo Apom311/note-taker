c6f7c34d83b9b4a1cced45b5edc35b02bd2df29f
Apon wuz here <frost_bird@hotmail.com>
2022-09-01 16:37:11 -0500

    fixed routing issues

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,12 @@
 const path = require("path");
+const router = require('express').Router();
 
-module.exports = function(app) {
+router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/index.html"));
-    });
+router.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/notes.html"));
-    });
-}
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 //Dependencies
 const express = require('express');
-const fs = require('fs');
-
-var app = express();
-var PORT = process.env.PORT || 5000;
+const app = express();
+const htmlRoutes = require('./routes/html-routes');
+const apiRoutes = require('./routes/api-routes')
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use("/assets", express.static("./public/assets"));
 
-require("./routes/html-routes")(app);
-require("./routes/api-routes")(app);
+app.use('/', htmlRoutes);
+app.use('/', apiRoutes);
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT" + PORT);
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
 });
